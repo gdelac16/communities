1a7991bc648eb9b04f8ceee877ce20c33b0e5626
gdelac16 <143772861+gdelac16@users.noreply.github.com>
2023-11-07 14:50:07 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='community',
-    version='2.1',
+    version='2.2',
     py_modules=['communities'],
     entry_points={
         "console_scripts": ["community=communities:run_script"]
